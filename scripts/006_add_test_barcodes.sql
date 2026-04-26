cf651e08b7132150392230e39ec459bd2c722c3a
Lucas Luna <rlluna15@hotmail.com>
2026-04-26 02:34:02 -0300

    atua

--- scripts/006_add_test_barcodes.sql
+++ scripts/006_add_test_barcodes.sql
@@ -1,7 +1,7 @@
 -- Adiciona codigos de barras em 3 produtos para teste
 -- Formato EAN-13 (padrao brasileiro)
 
--- Atualiza os 3 primeiros produtos ativos com codigos de barras
+-- Atualiza os 3 primeiros produtos ativos com codigos de barras ....
 UPDATE products
 SET barcode = '7891234567890'
 WHERE id = (SELECT id FROM products WHERE is_active = true ORDER BY created_at LIMIT 1 OFFSET 0);
